9ef5da027224700cfe09783a6cda4e8814a1400a
Marcin Brański <mbr@touk.pl>
2015-03-26 12:56:39 +0100

    resolve all the things!!

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 echo "$MESOS_IP mesos">> /etc/hosts
 sed -i.bak "s/MESOS_IP/$MESOS_IP/" /go/src/mesos-dns/config.json
+sed -i.bak "s/RESOLVER/$RESOLVER/" /go/src/mesos-dns/config.json
 /go/src/mesos-dns/mesos-dns -config /go/src/mesos-dns/config.json -v=2
